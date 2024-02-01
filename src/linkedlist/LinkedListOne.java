b05a55b961ed2feb428b118779fe186196283d53
KushalmyDesk <kushal99mondal@gmail.com>
2024-02-01 11:54:22 +0530

    Initial Commit

---
+++ src/linkedlist/LinkedListOne.java
@@ -0,0 +1,11 @@
+package linkedlist;
+
+/**
+ * linkedlistOne
+ */
+public class LinkedListOne {
+  public static void main(String[] args) {
+    System.out.println("This is LinkedListOne");
+  }
+
+}
---
+++ src/variables/Variables.java
@@ -0,0 +1,24 @@
+package variables;
+
+public class Variables {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Variables in Java");
+
+    // Declaration
+    int x;
+
+    // Assignment
+    x = 123;
+
+    // Initialization
+    int y = 123;
+
+    System.out.println("The Values of x and y is: " + x + "," + y);
+
+    double pi = 3.14;
+    char symbol = 'p';
+    String name = "Kushal";
+
+    System.out.println("double pi: " + pi + ", char symbol:" + symbol + ", String name:" + name);
+  }
+}
